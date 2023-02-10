2f20a2917c775cab37331085d563d190253d4194
lvy <48097166+lvvvvvy@users.noreply.github.com>
2023-02-10 03:35:30 +0000

    feat: add card pool

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
+#include "CardPool.h"
+
 #include <iostream>
+
+using namespace herokill;
 
 int main(void)
 {
     std::cout << "hello herokill" << std::endl;
+    std::cout << "card pool init:" << std::endl;
+    CardPool pools;
+    pools.dumpCards();
+    std::cout << "card pool shuffle:" << std::endl;
+    pools.shuffle();
+    pools.dumpCards();
+
     return 0;
 }
